e77ea401eed9f89e31fa0dc21e479f1660b7f3b5
tommywu10 <tomwu10@gmail.com>
2021-02-04 22:18:43 -0500

    mothacluck

--- src/Components/HomeComponents/HomePageImage.js
+++ src/Components/HomeComponents/HomePageImage.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function HomePageImage(){
     return (
         <div className="home-page-image-container">
-            <h1>Cluck cluck, mothafuck.</h1>
+            <h1>Cluck cluck, mothacluck.</h1>
         </div>
     )
 }
