bfd191eb8020317868b18ca1b745442b7a3634db
tommywu10 <tomwu10@gmail.com>
2021-02-12 20:16:05 -0500

    finally got api to work. styled shop page

--- src/Components/Shop.js
+++ src/Components/Shop.js
@@ -2,23 +2,49 @@ import React, {useEffect, useState} from 'react'
 import Header from './Header'
 import Footer from './Footer'
 
-import url from '../shopData'
 
 export default function Shop(){
     const [shopData, setShopData] = useState([])
+    
+    
 
     useEffect(() => {
-        fetch(url)
+        fetch('http://localhost:3333/chicken')
         .then(response => response.json())
         .then(data => setShopData(data))
+        console.log(shopData)
     }, [])
 
     return(
         <div>
             <Header />
-            {/* {shopData.map((item, index) => {
-                <p>{item.chicken}</p>
-            })} */}
+
+            <div className="shop-widget">
+                <h1>shop your favorite chicken</h1>
+                <p>Here's all your favorite choices for either cooking, raising, or slaughtering.</p>
+                <select>
+                    <option>Sort By</option>
+                    <option>Highest to lowest price</option>
+                    <option>Lowest to highest price</option>
+                    <option>Name</option>
+                </select>
+            </div>
+
+
+            <div className="shop-container">
+                {shopData.map((item, index)=> 
+                    <div className="shop-item-card" key={index}>
+                        <img src={item.image} alt={item.name}/>
+                        <div className="shop-item-card-bottom">
+                            <h3>{item.name}</h3>
+                            <p>{item.price}</p>
+                            <button>Purchase chicken</button>
+                        </div>
+                    </div>
+                    )
+                }
+            </div>
+            
             <Footer />
         </div>
     )
